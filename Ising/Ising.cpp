2dfeb112a110b5ea226085507fba6a13869c9acc
Merciful-jm <56538702+Merciful-jm@users.noreply.github.com>
2020-07-05 12:09:28 +0800

    for old Version to calculate the binder.

--- Ising/Ising.cpp
+++ Ising/Ising.cpp
@@ -1,10 +1,8 @@
-﻿ //
-#include <iostream>
+﻿#include <iostream>
 #include<cmath>
 #include <iomanip>
 #include<fstream>
 #include"Ising mode of function.h"
-#include"Ising mode of function.cpp"
 //#include"prngcl_ranecu.cl.h"
 #include <random>
 #include<stdlib.h>
@@ -16,26 +14,26 @@ using namespace std;
 
 
 double randomofjm()
-{	
+{
 	double r;
 	static default_random_engine e;
 	static uniform_real_distribution<double> u(0, 1);
 	r = u(e);
 	return  r;
-	//static double a;
-	//static double b;
-	//static double r;
-	//static random_device rd;
-	//a = rd();
-	//b = rd.max();
-	//r = (a / b);
-	//return r;
+	/*static double a;
+	static double b;
+	static double r;
+	static random_device rd;
+	a = rd();
+	b = rd.max();
+	r = (a / b);
+	return r;*/
 
 
 }
 
 
-int main (void)
+int main(void)
 {
 	//double T;
 	double sum = 0;
@@ -55,64 +53,24 @@ int main (void)
 	int InitialSpin = 1;
 	ofstream file0;
 	double s = 0;
-	
 
 	Magnetization = Magnetization_total(Spins);
-    file0.open("L_8Ising_uniformT0-5_0.05_2-3_0.01.txt");
+	file0.open("new2DIsingMode7.2L_32T0-0.3.txt");
 	file0 << "temperature \t <E> \t <E^2> \t <m> \t <m^2>" << endl;
-	//file0 << "## Monte Carlo Simulation for 2D Ising Model with Periodic Boundary conditions" << endl;
-	//file0 << "## Algorithm : Metropolis Algorithm" << endl;
-	//file0 << "## Random Number type: 	uniform_real_distribution<double> u(0, 1);" << endl;
-	//file0 << "## L = " << L << endl;
-	//file0 << "## Strength of the Magnetic Field: H = " << H << endl;
-	//file0 << "## Total Monte Carlo Time =  " << TotalTime << endl;
-	//file0 << "## Relaxation Time =  " << RelaxationTime << endl;
-	//file0 << "temperature"<<"\t"<<setw(15)<< " <E>" << "\t" << setw(13) << "<E^2>" << "\t" << setw(28) << " <m>" << "\t" << setw(30) << " <m^2> "<< endl;
-	Initialization( InitialSpin, Spins);
+	file0 << "## Monte Carlo Simulation for 2D Ising Model with Periodic Boundary conditions" << endl;
+	file0 << "## Algorithm : Metropolis Algorithm" << endl;
+	file0 << "## Random Number type: 	uniform_real_distribution<double> u(0, 1);" << endl;
+	file0 << "## L = " << L << endl;
+	file0 << "## Strength of the Magnetic Field: H = " << H << endl;
+	file0 << "## Total Monte Carlo Time =  " << TotalTime << endl;
+	file0 << "## Relaxation Time =  " << RelaxationTime << endl;
+	file0 << "temperature" << "\t" << setw(15) << " <E>" << "\t" << setw(13) << "<E^2>" << "\t" << setw(28) << " <m>" << "\t" << setw(30) << " <m^2> " << endl;
+	Initialization(InitialSpin, Spins);
 	Energy = Energy_total(Spins);
 	Magnetization = Magnetization_total(Spins);
-	for (double j=0.0; j < 5; j = j + 0.05)
+	for (double j = 0.0; j < 0.3; j = j + 0.01)
 	{
-		if (j >= 2.00 && j < 2.95)for (double jj = 0.0; jj < 0.05; jj = jj + 0.01)
-		{
-			sum = 0.0;
-			int  Number = 0;
-			sum_m = 0.0;
-			sum_m2 = 0.0;
-			sum_m4 = 0.0;
-			sum_e = 0.0;
-			sum_e2 = 0.0;
-			point_magnetization = 0.0;
-			T = j + jj;
-			cout << T << endl;
-			for (int k = 0; k < TotalTime; k++)
-			{
-				MonteCarloSweep(Spins, Energy, Magnetization, magnet2, energy2, T);
-				if (k >= RelaxationTime)
-				{
-					if ((k % 10) == 0)
-					{
-						Magnetization_perSpin = Magnetization / (double(L) * L);
-						//Magnetization_perSpin = fabs(Magnetization_perSpin);//no need to have a absolute value.
-						sum_m = sum_m + Magnetization_perSpin;
-						Energy_perSpin = Energy / (double(L) * L);
-						sum_e = sum_e + Energy_perSpin;
-						magnet2_perSpin = magnet2 / (double(L) * L * L * L);
-						sum_m2 = sum_m2 + magnet2_perSpin;
-						energy2_perSpin = energy2 / (double(L) * L * L * L);
-						sum_e2 = sum_e2 + energy2_perSpin;
 
-					}
-				}
-			}
-			file0 << T << "\t" << setw(30) << (sum_e / 10000.0) << "\t" << setw(30) << (sum_e2 / 10000.0) << "\t" << setw(30) << (sum_m / 10000.0) << "\t " << setw(30) << (sum_m2 / 10000.0) << "\t ";
-
-			//		file0 << T << "\t" << setw(30) <<  Binder*3/2.0 << "\t" << setw(30) << (sum_m / 10000.0) << "\t"<< setw(30) << (sum_m2 / 10000.0) << "\t "<< setw(30) << (sum_m4 / 10000.0) << "\t ";
-			file0 << endl;
-
-		}
-		else
-		{
 		sum = 0.0;
 		int  Number = 0;
 		sum_m = 0.0;
@@ -122,7 +80,9 @@ int main (void)
 		sum_e2 = 0.0;
 		point_magnetization = 0.0;
 		T = j;
-		cout << T << endl;
+		cout << j << endl;
+		//		double total_m2 = 0, total_m4 = 0, total_e2 = 0;
+
 		for (int k = 0; k < TotalTime; k++)
 		{
 			MonteCarloSweep(Spins, Energy, Magnetization, magnet2, energy2, T);
@@ -140,6 +100,10 @@ int main (void)
 					energy2_perSpin = energy2 / (double(L) * L * L * L);
 					sum_e2 = sum_e2 + energy2_perSpin;
 
+
+
+
+
 				}
 			}
 		}
@@ -148,10 +112,8 @@ int main (void)
 		//		file0 << T << "\t" << setw(30) <<  Binder*3/2.0 << "\t" << setw(30) << (sum_m / 10000.0) << "\t"<< setw(30) << (sum_m2 / 10000.0) << "\t "<< setw(30) << (sum_m4 / 10000.0) << "\t ";
 		file0 << endl;
 
-		}
-
 	}
-	
+
 	file0.close();
 	return 0;
 }
